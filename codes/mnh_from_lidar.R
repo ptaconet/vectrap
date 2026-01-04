ff571fb778658796ca25ae3ccfc28fa65db0d90b
Paul Taconet <paul.taconet@gmail.com>
2026-01-04 14:27:22 +0100

    update codes

---
+++ codes/mnh_from_lidar.R
@@ -0,0 +1,45 @@
+library(lidR)
+library(sf)
+
+dalles_lidar_hd_mtp <- st_read("data/lidar_hd/dalles_lidar_hd_mtp.gpkg")
+
+urls <- dalles_lidar_hd_mtp$url
+
+for (url in urls) {
+  # Extract the filename from the URL
+  filename <- basename(url)
+  destfile <- paste0("/home/ptaconet/contributions_diverses_projets_mivegec/vectrap/data/lidar_hd/raw_lidar_files/",filename)
+
+  # Download the file
+  download.file(url, destfile = destfile, mode = "wb")
+
+  las <- readLAS(destfile)
+
+  mnt = grid_terrain(las, 0.25, knnidw())
+  mns = grid_canopy(las, 0.25, p2r())
+  mnh <- mns - mnt
+  mnh <- terra::rast(mnh)
+
+  mnh2 <- terra::focal(mnh, w=5, fun=mean, na.policy="only")
+
+  terra::writeRaster(mnh,paste0("/home/ptaconet/contributions_diverses_projets_mivegec/vectrap/data/lidar_hd/mnh_25cm/",gsub(".copc.laz",".tif",filename)),overwrite=TRUE)
+
+}
+
+
+
+# --- Step 4: Load all GeoTIFFs ---
+tif_files <- list.files("/home/ptaconet/contributions_diverses_projets_mivegec/vectrap/data/lidar_hd/mnh_25cm", pattern = "\\.tif$", full.names = TRUE)
+rasters <- lapply(tif_files, rast)
+
+# --- Step 5: Merge (mosaic) all tiles ---
+mosaic_raster <- do.call(mosaic, rasters)
+
+# --- Step 6: Save the merged raster ---
+writeRaster(mosaic_raster, "/home/ptaconet/contributions_diverses_projets_mivegec/vectrap/data/lidar_hd/mnh_25cm/merged_lidar_mosaic_25cm.tif", overwrite = TRUE)
+
+cat("✅ Mosaic saved as merged_lidar_mosaic.tif\n")
+
+
+
+
--- codes/segment_otb.R
+++ codes/segment_otb.R
@@ -2,6 +2,11 @@ library(terra)
 library(sf)
 library(dplyr)
 
+path_to_processing_folder <- "/home/ptaconet/contributions_diverses_projets_mivegec/vectrap/data/ortho_montpellier/traitements"
+path_to_output_folder <- "/home/ptaconet/contributions_diverses_projets_mivegec/vectrap/data/ortho_montpellier/output_classif"
+source_otb_call <- "source /home/ptaconet/Téléchargements/OTB-9.1.0-Linux/otbenv.profile && "  #Source the OTB environment (very important) to be done directly in the shell
+
+
 files <- list.files("/home/ptaconet/contributions_diverses_projets_mivegec/vectrap/data/ortho_montpellier/IRC_secteur_zoo", pattern = "tif", full.names = TRUE)
 files <- files[-which(grepl("aux.xml", files))]
 
@@ -52,39 +57,104 @@ th_site_files <- intersections %>%
 
 th_site_files <- th_site_files$file
 
-for(i in 1:length(th_site_files))
 
-# rasters <- sprc(th_site_files)
-# r <- merge(rasters)
-#
 
-#Source the OTB environment (very important) to be done directly in the shell
-system("source /home/ptaconet/Téléchargements/OTB-9.1.0-Linux/otbenv.profile")
 
-path_to_ndvi <- "/home/ptaconet/ndvi.tif"
-otb_appli<-paste0("otbcli_RadiometricIndices -in ", th_site_files," -channels.red 2 -channels.nir 1 -list Vegetation:NDVI -out ",path_to_ndvi)
-system(otb_appli)
+for(i in 1:length(th_site_files)){
 
-path_to_segmented <- "/home/ptaconet/segmented.tif"
-otb_appli<-paste0("otbcli_GenericRegionMerging -in ", path_to_ndvi," -criterion bs -threshold 23 -speed 1 -cw 0.96 -sw 0.04 -out ",path_to_segmented)
-system(otb_appli)
+th_site_file <- th_site_files[i]
 
-gdal_appli<-paste0("gdal_polygonize.py ", path_to_segmented," ",gsub(".tif", ".gpkg", path_to_segmented)," -b 1")
+tile_name = gsub(".tif","",basename(th_site_file))
+
+cat("processing tile", tile_name, "..." )
+
+# mosaiquer
+# cmd<-paste0(source_otb_call,"otbcli_Mosaic -il ",paste(th_site_files, collapse = " ")," -comp.feather none -harmo.method none -out /home/ptaconet/mosaic.tif")
+# system(paste("bash -c", shQuote(cmd)))
+
+tictoc::tic()
+
+#calculer ndvi
+cmd <- paste0(source_otb_call,"otbcli_RadiometricIndices -in ", th_site_file," -channels.nir 1 -channels.red 2 -channels.green 3 -list Vegetation:NDVI -out ",path_to_processing_folder,"/ndvi.tif")
+#cmd <- paste0(source_otb_call,"otbcli_RadiometricIndices -in /home/ptaconet/mosaic.tif -channels.nir 1 -channels.red 2 -channels.green 3 -list Vegetation:NDVI -out /home/ptaconet/ndvi.tif")
+system(paste("bash -c", shQuote(cmd)))
+
+# rescaler ndvi entre 0 et 255
+cmd <- paste0(source_otb_call,'otbcli_BandMathX -il ',path_to_processing_folder,'/ndvi.tif -out ',path_to_processing_folder,'/ndvi_255.tif -exp "((im1b1 + 1) / 2) * 255"')
+system(paste("bash -c", shQuote(cmd)))
+
+# recuperer vert et nir de IRC
+cmd <- paste0(source_otb_call,'otbcli_BandMathX -il ', th_site_file,' -out ',path_to_processing_folder,'/green_nir.tif -exp "im1b1; im1b3"')
+#cmd <- paste0(source_otb_call,'otbcli_BandMathX -il /home/ptaconet/mosaic.tif -out /home/ptaconet/green_nir.tif -exp "im1b1; im1b3"')
+system(paste("bash -c", shQuote(cmd)))
+
+# merger ndvi, nir, green (dabord splitter IRC puis concatener)
+cmd <- paste0(source_otb_call,'otbcli_ConcatenateImages -il ',path_to_processing_folder,'/green_nir.tif ',path_to_processing_folder,'/ndvi_255.tif -out ',path_to_processing_folder,'/green_nir_ndvi.tif')
+system(paste("bash -c", shQuote(cmd)))
+
+# mean shift segmentation
+cmd <- paste0(source_otb_call,"otbcli_LargeScaleMeanShift -in ",path_to_processing_folder,"/green_nir_ndvi.tif -spatialr 10 -ranger 20 -minsize 800 -ram 8000 -mode vector -mode.vector.out ",path_to_processing_folder,"/segment.gpkg")
+system(paste("bash -c", shQuote(cmd)))
+
+#  reprojeter en EPSG:2154
+gdal_appli<-paste0("ogr2ogr -a_srs EPSG:2154 ",path_to_processing_folder,"/segment_project.gpkg ",path_to_processing_folder,"/segment.gpkg")
 system(gdal_appli)
 
-# ==== 3. Compute NDVI ====
-red <- r[[2]]
-nir <- r[[1]]
+# ajouter la hauteur dans les covariables
+cmd <- paste0(source_otb_call,"otbcli_ZonalStatistics -in /home/ptaconet/contributions_diverses_projets_mivegec/vectrap/data/lidar_hd/mnh_filled.tif -inzone.vector.in ",path_to_processing_folder,"/segment_project.gpkg -out.vector.filename ",path_to_output_folder,"/",tile_name,".gpkg")
+system(paste("bash -c", shQuote(cmd)))
 
-ndvi <- (nir - red) / (nir + red)
+file.remove(list.files(path_to_processing_folder, recursive = TRUE, full.names = T))
 
-writeRaster(ndvi,"/home/ptaconet/ndvi2.tif", overwrite = T)
+tictoc::toc()
+
+}
+
+
+
+# ouvrir le fichier vecteur
+segment <- st_read(paste0(path_to_processing_folder,"/segment_final.gpkg"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# path_to_mosaic <- "/home/ptaconet/mosaic.tif"
+# otb_appli<-paste0("otbcli_Mosaic -il ",paste(th_site_files, collapse = " ")," -comp.feather none -harmo.method none -out ",path_to_mosaic," uint8")
+# system(otb_appli)
+
+cmd <- paste0(source_otb_call,"otbcli_RadiometricIndices -in ", th_site_files," -channels.nir 1 -channels.red 2 -channels.green 3 -list Vegetation:NDVI -out /home/ptaconet/ndvi.tif")
+system(paste("bash -c", shQuote(cmd)))
+
+# then Apply spatial smoothing to NDVI before segmentation
+cmd <- paste0(source_otb_call,"otbcli_MeanShiftSmoothing -in /home/ptaconet/ndvi.tif -spatialr 20 -ranger 0.15 -ram 8000 -fout /home/ptaconet/ndvi_smooth.tif")
+system(paste("bash -c", shQuote(cmd)))
+
+# segment
+cmd <- paste0(source_otb_call,"otbcli_GenericRegionMerging -in /home/ptaconet/ndvi_smooth.tif -criterion bs -threshold 20 -speed 1 -cw 0.97 -sw 0.03 -out /home/ptaconet/segmented.tif")
+system(paste("bash -c", shQuote(cmd)))
+
+# set src
+gdal_appli<-"gdal_edit.py /home/ptaconet/segmented.tif -a_srs EPSG:2154"
+system(gdal_appli)
+
+# polygonize
+gdal_appli<-"gdal_polygonize.py /home/ptaconet/segmented.tif /home/ptaconet/segmented.gpkg -b 1"
+system(gdal_appli)
+
 
 #####" parametres OTB qui fonctionnent bien pour la segmentation
 
-system(otb_appli)
-
-# Algo GenericRegionMerging
 # sur image NDVI
 # threshold = 23
 # spectral = 0.96
